8dab155ac0c9c14c999e5368f9b3ab1a75622e74
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2019-08-06 19:46:21 +0200

    Setting dynamic port

--- src/main/kotlin/it/plague/blog/verticle/HelloVerticle.kt
+++ src/main/kotlin/it/plague/blog/verticle/HelloVerticle.kt
@@ -6,10 +6,11 @@ import io.vertx.core.Promise
 class HelloVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>?) {
+    val port: String = System.getenv("PORT") ?: "8080"
     vertx
       .createHttpServer()
       .requestHandler { r -> r.response().end("<h1>Hello from my first " + "Vert.x 3 application</h1>") }
-      .listen(8080) { result ->
+      .listen(port.toInt()) { result ->
         if (result.succeeded()) {
           startPromise?.complete()
         } else {
