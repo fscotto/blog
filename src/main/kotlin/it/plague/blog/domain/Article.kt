88854d2db1685efb7510e7e6a93b27712be857a0
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2019-08-14 14:32:30 +0200

    Json converter

--- src/main/kotlin/it/plague/blog/domain/Article.kt
+++ src/main/kotlin/it/plague/blog/domain/Article.kt
@@ -1,16 +1,14 @@
 package it.plague.blog.domain
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import io.vertx.core.buffer.Buffer
 import java.time.LocalDateTime
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Article(var id: Long? = 0,
 									 var createdBy: User,
-									 var created: LocalDateTime = LocalDateTime.now(),
+									 var created: LocalDateTime = LocalDateTime.now().withNano(0),
 									 var modifiedBy: User? = null,
 									 var modified: LocalDateTime? = null,
 									 var author: Author,
 									 var title: String,
-									 @JsonIgnore var content: Buffer? = null)
+									 var content: String = "")
--- src/test/kotlin/it/plague/blog/util/JsonConvertTest.kt
+++ src/test/kotlin/it/plague/blog/util/JsonConvertTest.kt
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.time.LocalDateTime
-import java.time.Month
 import java.time.format.DateTimeFormatter
 
 class JsonConvertTest {
@@ -21,35 +20,37 @@ class JsonConvertTest {
 	@BeforeEach
 	fun setUp() {
 		val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-		val now = LocalDateTime.now()
+		val now = LocalDateTime.now().withNano(0)
 		this.jsonArticle = JsonObject().apply {
-			put("id", "1")
+			put("id", 1)
 			put("createdBy", JsonObject()
-				.put("id", "1")
+				.put("id", 1)
 				.put("username", "joe")
 				.put("password", "secret"))
 			put("created", formatter.format(now))
 			putNull("modifiedBy")
 			putNull("modified")
 			put("author", JsonObject()
+				.put("id", 1)
 				.put("name", "J.R.")
 				.put("lastName", "Tolkien")
 			)
 			put("title", "Lord of the rings")
+			put("content", "")
 		}
 
 		this.article = Article(
 			id = 1,
 			createdBy = User(id = 1, username = "joe", password = "secret"),
 			created = now,
-			author = Author(name = "J.R.", lastName = "Tolkien"),
+			author = Author(id = 1, name = "J.R.", lastName = "Tolkien"),
 			title = "Lord of the rings"
 		)
 	}
 
 	@Test
 	fun `should be convert from json to object`() {
-		val actual = this.jsonArticle?.let { obj -> JsonConverter.fromJson(obj, Article::class.java) }
+		val actual = this.jsonArticle?.let { json -> JsonConverter.fromJson(json, Article::class.java) }
 		Assertions.assertThat(actual).isEqualTo(this.article)
 	}
 
@@ -71,7 +72,6 @@ class JsonConvertTest {
 	fun `should be throw MissingKotlinParameterException with malformed json`() {
 		assertThrows<MissingKotlinParameterException> {
 			val jsonObject = JsonObject().apply {
-				put("created", LocalDateTime.of(2019, Month.AUGUST, 12, 0, 0).toString())
 				putNull("modifiedBy")
 				putNull("modified")
 				put("author", JsonObject()
@@ -81,20 +81,12 @@ class JsonConvertTest {
 				put("title", "Lord of the rings")
 			}
 			JsonConverter.fromJson(jsonObject, Article::class.java)
-		}.also { exception ->
-			Assertions.assertThat(exception.message)
-				.isEqualTo(
-					"Instantiation of [simple type, class it.plague.blog.domain.Article] value failed for JSON property createdBy due " +
-						"to missing (therefore NULL) value for creator parameter createdBy which is a non-nullable type\n" +
-						" at [Source: (String)\"{\"created\":\"2019-08-12T00:00\",\"modifiedBy\":null,\"modified\":null,\"author\":{\"name\":\"J.R.\",\"lastName\":\"Tolkien\"},\"title\":\"Lord of the rings\"}\"; " +
-						"line: 1, column: 138] (through reference chain: it.plague.blog.domain.Article[\"createdBy\"])"
-				)
 		}
 	}
 
 	@Test
 	fun `should be convert from object to json`() {
-		val actual = this.article?.let { json -> JsonConverter.toJson(json) }
+		val actual = this.article?.let { obj -> JsonConverter.toJson(obj) }
 		Assertions.assertThat(actual).isEqualTo(jsonArticle)
 	}
 }
