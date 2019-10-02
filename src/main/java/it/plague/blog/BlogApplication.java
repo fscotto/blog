5fc300024df0b5eb82ed1043b4a10f5e9c312959
Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
2019-10-02 20:32:59 +0200

    Refactor main class

--- src/main/java/it/plague/blog/BlogApplication.java
+++ src/main/java/it/plague/blog/BlogApplication.java
@@ -1,9 +1,6 @@
 package it.plague.blog;
 
-import com.google.inject.Guice;
-import com.google.inject.Module;
 import io.vertx.core.VertxOptions;
-import io.vertx.core.spi.VerticleFactory;
 import io.vertx.reactivex.core.Vertx;
 import it.plague.blog.config.guice.*;
 import it.plague.blog.database.ArticleDatabaseVerticle;
@@ -17,37 +14,20 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class BlogApplication {
 
-  private static Vertx vertx;
-  private static VerticleFactory factory;
-
   public static void main(String[] args) {
-    log.info("Start Vert.x Home Blog");
-    vertx = Vertx.vertx(getOptions());
-    guiceBootstrap();
-    deployVerticle(ArticleWebVerticle.class.getName());
-    deployVerticle(ArticleDatabaseVerticle.class.getName());
-    deployVerticle(InformationWebVerticle.class.getName());
-    deployVerticle(InformationDatabaseVerticle.class.getName());
-  }
-
-  private static void guiceBootstrap() {
-    var injector = Guice.createInjector(getModules());
-    factory = new GuiceVerticleFactory(injector);
-    vertx.registerVerticleFactory(factory);
-  }
-
-  private static Module[] getModules() {
-    return new Module[]{
-      new VertxModule(vertx),
-      new ConfigModule(),
-      new WebModule(vertx),
-      new DatabaseModule(vertx)
-    };
-  }
-
-  private static void deployVerticle(String verticleName) {
-    log.debug(String.format("Deploy verticle with name %s", verticleName));
-    vertx.deployVerticle(factory.prefix() + ":" + verticleName);
+    log.info("Start Plague's Blog");
+    var vertx = Vertx.vertx(getOptions());
+    GuiceRunner.newInstance(vertx)
+      .registerModule(new VertxModule(vertx))
+      .registerModule(new ConfigModule())
+      .registerModule(new WebModule(vertx))
+      .registerModule(new DatabaseModule(vertx))
+      .deployVerticle(ArticleWebVerticle.class.getName())
+      .deployVerticle(ArticleDatabaseVerticle.class.getName())
+      .deployVerticle(InformationWebVerticle.class.getName())
+      .deployVerticle(InformationDatabaseVerticle.class.getName())
+      .start();
+    log.info("Plague's Blog started");
   }
 
   private static VertxOptions getOptions() {
