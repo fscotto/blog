9feb72888e662d2583f54c088d6566ef296fdfc9
Fabio Scotto di Santolo <fabio.scottodisantolo@nttdata.com>
2019-12-10 16:21:59 +0100

    add module for frontend with Vue.js

---
+++ frontend/vue.config.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2019 Fabio Scotto di Santolo <fabio.scottodisantolo@gmail.com>
+ *
+ * This file is part of plague-blog.
+ *
+ * plague-blog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * plague-blog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with plague-blog.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+  // FIXME: add proxy configuration (maybe)
+  // change build paths to make them Maven compatible
+  // see https://cli.vuejs.org/config/
+  outputDir: 'target/dist',
+  assetsDir: 'static'
+}
